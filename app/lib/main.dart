ea43675c609afe8f87fcb8642988a9375c8ba9c5
jasonTelanoff <jason.telanoff@gmail.com>
2020-05-31 23:42:47 -0700

    Added more questions

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,67 +1,58 @@
 import 'package:flutter/material.dart';
 
+List restart = ['restart', 'restart'];
+List noEnter = ['noEnter', 'noEnter'];
+
 Map questions = {
-  'restart': [
-    {
+  'restart': {
+    'restart': {
       'question': 'Do you want to restart?',
       'answers': ['Yes', 'Yes'],
       'functions': [
-        ['enter', 0],
-        ['enter', 0]
+        ['enter', 'enter'],
+        ['enter', 'enter']
       ]
     },
-  ],
-  'enter': [
-    {
+  },
+  'enter': {
+    'enter': {
       'question': 'Do you want to enter the cave?',
       'answers': ['Yes', 'No'],
       'functions': [
-        ['dragonFight', 0],
-        ['noEnter', 0]
+        ['dragonFight', 'start'],
+        noEnter
       ]
     },
-  ],
-  'noEnter': [
-    {
-      'question': 'The game doesn\'s even start, and thus is over.',
+  },
+  'noEnter': {
+    'noEnter': {
+      'question': 'The game doesn\'t even start, and thus is over.',
       'answers': ['Restart'],
       'functions': [
-        ['restart', 0],
+        restart,
       ]
     },
-  ],
-  'dragonFight': [
-    // 0
-    {
+  },
+  'dragonFight': {
+    'start': {
       'question': 'You see a giant dragon, do you approach?',
       'answers': ['Yes, I is brave', 'Aw hell nah'],
       'functions': [
-        ['dragonFight', 1],
-        ['dragonFight', 2]
+        ['dragonFight', 'fight'],
+        ['egg', 'start']
       ]
     },
-    // 1
-    {
+    'fight': {
       'question':
           'You can see a cut in its side. And some smoke exit its nose.',
       'answers': ['Stab it there, it\s weak', 'Turn around, that sh*t crazy'],
       'functions': [
-        ['dragonFight', 3],
-        ['dragonFight', 2]
-      ]
-    },
-    // 2
-    {
-      'question': 'You turn and see a rather large egg.',
-      'answers': ['Ooh! Dinner', 'Avoid it', 'Check its size'],
-      'functions': [
-        ['dragonFight', 4],
-        ['dragonFight', 7],
-        ['dragonFight', 8],
+        ['dragonFight', 'attack'],
+        ['egg', 'start']
       ]
     },
     // 3
-    {
+    'attack': {
       'question':
           'You lunge, and hear a defeaning scream, as fire engulfs your surroundings.',
       'answers': [
@@ -69,137 +60,239 @@ Map questions = {
         'Try to escape while you still can'
       ],
       'functions': [
-        ['dragonFight', 11],
-        ['dragonFight', 12]
+        ['dragonFight', 'kill'],
+        ['dragonFight', 'burn']
       ]
     },
-    // 4
-    {
-      'question':
-          'You crack the egg open, and pull out a pop-up stove from your backpack.',
-      'answers': ['Cook it', 'Be a man and eat it raw'],
-      'functions': [
-        ['dragonFight', 5],
-        ['dragonFight', 6]
-      ]
-    },
-    // 5
-    {
-      'question':
-          'You start cooking the egg, when the dragon smells it. It comes, and eats you.',
-      'answers': ['Aww man'],
-      'functions': [
-        ['restart', 0]
-      ]
-    },
-    // 6
-    {
-      'question':
-          'You eat it, and gain God-like powers. You now become the ultimate ruler of the universe.',
-      'answers': ['Sick', 'Radical', 'Me?', 'Holy Frick'],
-      'functions': [
-        ['restart', 0],
-        ['restart', 0],
-        ['restart', 0],
-        ['restart', 0],
-      ]
-    },
-    // 7
-    {
-      'question': 'You avoid it, and fall in a trap.',
-      'answers': ['sad'],
-      'functions': [
-        ['restart', 0],
-      ]
-    },
-    // 8
-    {
-      'question': 'You take out your tape measure.',
-      'answers': ['Use it', 'Eyeball it'],
-      'functions': [
-        ['dragonFight', 9],
-        ['dragonFight', 10],
-      ]
-    },
-    // 9
-    {
-      'question':
-          'You measure it, and find it\'s five feet in circumference. God also needs to smite you for knowing this information.',
-      'answers': ['Ooh, God', 'At least I know now'],
-      'functions': [
-        ['restart', 0],
-        ['restart', 0],
-      ]
-    },
-    // 10
-    {
-      'question': 'You guess it\'s about 6 fett tall?',
-      'answers': ['Cool, I\'m done here', 'Look for more'],
-      'functions': [
-        ['restart', 0],
-        ['dragonFight', 13]
-      ]
-    },
-    // 11
-    {
+    'kill': {
       'question':
           'You end up killing it, and PETA is now after you. Good luck.',
       'answers': ['Thanks, I\'ll need it', 'PETA can\'t touch me'],
       'functions': [
-        ['PETA', 0],
-        ['PETA', 0],
+        ['PETA', 'start'],
+        ['PETA', 'start'],
       ]
     },
-    // 12
-    {
+    'burn': {
       'question': 'It didn\'t work, and you get burned alive',
       'answers': ['Owweee'],
       'functions': [
-        ['restart', 0],
+        restart,
       ]
     },
-    // 13
-    {
-      'question': 'You find another egg.',
-      'answers': ['Compare sizes with the other egg'],
-      'functions': [
-        ['dragonFight', 14],
-      ]
-    },
-    // 14
-    {
-      'question': 'You see this one is slightly larger.',
-      'answers': ['Size doesn\'t matter'],
-      'functions': [
-        ['dragonFight', 15],
-      ]
-    },
-    // 15
-    {
-      'question': 'Yes it does.',
-      'answers': ['Ok'],
-      'functions': [
-        ['restart', 0],
-      ]
-    },
-  ],
-  'PETA': [
-    {
+  },
+  'PETA': {
+    'start': {
       'question': 'PETA catches you, and does unspeakable things',
       'answers': ['Aww', 'Did you just run out of things to say?'],
       'functions': [
-        ['restart', 0],
-        ['PETA', 1],
+        restart,
+        ['PETA', 'runOut'],
       ]
     },
-    {
+    'runOut': {
       'question': 'Yes',
       'answers': ['Solid'],
       'functions': [
-        ['restart', 0],
+        restart,
       ]
     },
-  ]
+  },
+  'trap': {
+    'start': {
+      'question': 'You avoid it, and fall in a hole. You can\'t see anything',
+      'answers': [
+        'Feel around for a lightswitch',
+        'Just sit there',
+        'Open your eyes'
+      ],
+      'functions': [
+        ['trap', 'feel'],
+        ['trap', 'sit'],
+        ['trap', 'open'],
+      ]
+    },
+    'sit': {
+      'question': 'You sit and wait',
+      'answers': [
+        'Meditate',
+        'Try to sleep',
+      ],
+      'functions': [
+        ['trap', 'meditate'],
+        ['trap', 'sleep'],
+      ]
+    },
+    'sleep': {
+      'question': 'You sleep, and awaken in the very far future',
+      'answers': [
+        'Yeah, that\'s about it. I\'m done',
+      ],
+      'functions': [
+        restart,
+      ]
+    },
+    'meditate': {
+      'question': 'You reach enlightenment, but die of thirst shortly after.',
+      'answers': [
+        'Aww',
+        '*drinking own pee* No I didn\'t',
+      ],
+      'functions': [
+        restart,
+        ['trap', 'pee'],
+      ]
+    },
+    'pee': {
+      'question': 'You lose enloghtenment and all respect for you.',
+      'answers': [
+        'I know',
+      ],
+      'functions': [
+        restart,
+      ]
+    },
+    'feel': {
+      'question': 'You feel around, and stab your hand on a spike',
+      'answers': ['I know first aid', 'It\'s not that bad, I can push through'],
+      'functions': [
+        ['trap', 'aid'],
+        ['trap', 0],
+      ]
+    },
+    'aid': {
+      'question':
+          'You take off your shirt, and your chiseled abbs scare off all the bad things, and your wound heals. Everything in the universe now bows down to you.',
+      'answers': [
+        'I knew my working out would pay off',
+        'My... ABBS????? HAHAHAHAAHA, what abbs?'
+      ],
+      'functions': [restart, restart]
+    },
+    'open': {
+      'question': 'You open your eyes, and see the hole is ten feet deep.',
+      'answers': ['Look in your backpack for items', 'I can jump'],
+      'functions': [
+        ['trap', 'look'],
+        ['trap', 'jump'],
+      ]
+    },
+    'look': {
+      'question': 'You look in your backpack and find a 7 course meal for two.',
+      'answers': ['Good, I\'m starving', 'Look for more'],
+      'functions': [
+        ['trap', 'eat'],
+        ['trap', 'mirror'],
+      ]
+    },
+    'eat': {
+      'question':
+          'You begin eating, but remember you\re the one who cooked it. You die from food poisoning.',
+      'answers': [
+        'Aww, why couldn\'t it be my mom',
+        'I always knew I\'d go out this way'
+      ],
+      'functions': [restart, restart]
+    },
+    'mirror': {
+      'question': 'You find a mirror',
+      'answers': ['Ok, cool', 'I always knew I\'d go out this way'],
+      'functions': [restart, restart]
+    },
+    'jump': {
+      'question':
+          'You jump with the might of a thousand rabbits, and leave the hole.',
+      'answers': [
+        'Go fight the dragon',
+        'Go back to the egg',
+        'Holy sh*t, that was fun'
+      ],
+      'functions': [restart, restart, restart]
+    },
+  },
+  'egg': {
+    'start': {
+      'question': 'You turn and see a rather large egg.',
+      'answers': ['Ooh! Dinner', 'Avoid it', 'Check its size'],
+      'functions': [
+        ['egg', 'eat'],
+        ['trap', 'start'],
+        ['egg', 'measure'],
+      ]
+    },
+    'eat': {
+      'question':
+          'You crack the egg open, and pull out a pop-up stove from your backpack.',
+      'answers': ['Cook it', 'Be a man and eat it raw'],
+      'functions': [
+        ['egg', 'cook'],
+        ['egg', 'raw']
+      ]
+    },
+    'cook': {
+      'question':
+          'You start cooking the egg, when the dragon smells it. It comes, and eats you.',
+      'answers': ['Aww man'],
+      'functions': [restart]
+    },
+    'raw': {
+      'question':
+          'You eat it, and gain God-like powers. You now become the ultimate ruler of the universe.',
+      'answers': ['Sick', 'Radical', 'Me?', 'Holy Frick'],
+      'functions': [
+        restart,
+        restart,
+        restart,
+        restart,
+      ]
+    },
+    'measure': {
+      'question': 'You take out your tape measure.',
+      'answers': ['Use it', 'Eyeball it'],
+      'functions': [
+        ['egg', 'tape'],
+        ['egg', 'eyeball'],
+      ]
+    },
+    'tape': {
+      'question':
+          'You measure it, and find it\'s five feet in circumference. God also needs to smite you for knowing this information.',
+      'answers': ['Ooh, God', 'At least I know now'],
+      'functions': [
+        restart,
+        restart,
+      ]
+    },
+    'eyeball': {
+      'question': 'You guess it\'s about 6 feet tall?',
+      'answers': ['Cool, I\'m done here', 'Look for more'],
+      'functions': [
+        restart,
+        ['egg', 'look']
+      ]
+    },
+    'look': {
+      'question': 'You find another egg.',
+      'answers': ['Compare sizes with the other egg'],
+      'functions': [
+        ['egg', 'larger'],
+      ]
+    },
+    'larger': {
+      'question': 'You see this one is slightly larger.',
+      'answers': ['Size doesn\'t matter'],
+      'functions': [
+        ['egg', 'yes'],
+      ]
+    },
+    'yes': {
+      'question': 'Yes it does.',
+      'answers': ['Ok'],
+      'functions': [
+        restart,
+      ]
+    },
+  }
 };
 
 void main() {
@@ -227,13 +320,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String time;
-  int index;
+  String index;
   int itemCount;
 
   @override
   void initState() {
     super.initState();
-    index = 0;
+    index = 'enter';
     time = 'enter';
     itemCount = questions[time][index]['answers'].length;
   }
